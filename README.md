b00810573145d67d02f9ef93acab7bfc78b1861f
Matthew P Hamilton <30450935+matthewphamilton@users.noreply.github.com>
2024-12-05 14:55:46 +1100

    safety

--- README.md
+++ README.md
@@ -2,5 +2,26 @@
 
 This is the shell of a reporting template to be used in conjunction with the avert computational model.
 
+This reporting template is designed for use in conjunction with the `avert` R library. You can install `avert` with the following commands.
 
+``` r
+utils::install.packages("remotes")
 
+remotes::install_github("avertableburden/avert")
+```
+
+To render a report using the latest release of this template:
+
+``` r
+library(avert)
+write_avert_report(get_input_data(), output_dir_1L_chr = tempdir())
+```
+
+Alternatively, if you clone this repository to your machine, you can render the latest development version of the template from within the parent directory of the cloned repository:
+
+``` r
+library(avert)
+write_avert_report(get_input_data(), mkdn_source_dir_1L_chr = getwd(), output_dir_1L_chr = "Tests")
+```
+
+An example of a PDF report produced with the above calls is available for download [here](https://github.com/avertableburden/avert_report/releases/download/v0.1.1/Example_Report.pdf).
--- Tests/Render.R
+++ Tests/Render.R
@@ -1,82 +1,32 @@
-write_report <- function(results_ls,
-                         consent_1L_chr = "",
-                         consent_indcs_int = 1L,
-                         markdown_dir_1L_chr = character(0),
-                         mkdn_source_dir_1L_chr = NA_character_,
-                         options_chr = c("Y", "N"),
-                         output_dir_1L_chr = character(0)){
-  if(identical(markdown_dir_1L_chr, character(0))|identical(output_dir_1L_chr, character(0))){
-    temp_dir_1L_chr <- tempdir()
-    if(identical(markdown_dir_1L_chr, character(0))){
-      markdown_dir_1L_chr <- paste0(temp_dir_1L_chr, "/Markdown")
+library(avert)
+write_avert_report <- function (results_ls, consent_1L_chr = "", consent_indcs_int = 1L, 
+          markdown_dir_1L_chr = character(0), mkdn_source_dir_1L_chr = NA_character_, 
+          options_chr = c("Y", "N"), output_dir_1L_chr = character(0)) 
+{
+      if (identical(output_dir_1L_chr, character(0))) {
+      output_dir_1L_chr <- tempdir()
     }
-    if(identical(output_dir_1L_chr, character(0))){
-      output_dir_1L_chr <- paste0(temp_dir_1L_chr, "/Output")
-    }
-  }
-  X <- ready4show::Ready4showSynopsis(background_1L_chr = "The data reported in this document is entirely fictional.",
-                                      coi_1L_chr = "None declared.",
-                                      conclusion_1L_chr = "These fake results are not to be used to inform decision making.",
-                                      digits_int = 3L,
-                                      ethics_1L_chr = "No ethics approval has been received for the work summarised in this report.",
-                                      funding_1L_chr = "No funding information is available for this report.",
-                                      interval_chr = NA_character_,
-                                      keywords_chr = c("entirely","fake","do", "not","cite"),
-                                      outp_formats_chr = "PDF",
-                                      sample_desc_1L_chr = NA_character_,
-                                      title_1L_chr = "A hypothetical report from the avert compuational model")
-  X <- renewSlot(X,
-                 "authors_r3",
-                 first_nm_chr = "Anonymous",
-                 middle_nm_chr = "",
-                 last_nm_chr = "Author",
-                 title_chr = "",
-                 qualifications_chr = "",
-                 institute_chr = "Institute_A",
-                 sequence_int = 1,
-                 is_corresponding_lgl = T,
-                 email_chr = "fake_email@fake_institute.com")  
-  X <- renewSlot(X,
-                 "institutes_r3", short_name_chr = "Institute_A", long_name_chr = "Anonymous Institute") 
-  #
- X <- renewSlot(X,"a_Ready4showPaths@outp_data_dir_1L_chr",
-              temp_dir_1L_chr)
- X <- renewSlot(X, "a_Ready4showPaths@mkdn_source_dir_1L_chr", mkdn_source_dir_1L_chr)
-  authorData(X,
-             consent_1L_chr = consent_1L_chr,
-             consent_indcs_int = consent_indcs_int,
-             options_chr = options_chr,
-             tmpl_url_1L_chr = "https://github.com/avertableburden/avert_report",
-             tmpl_version_1L_chr = "0.1.2",
-             what_1L_chr = "Manuscript")
-  authorReport(X, consent_1L_chr = consent_1L_chr, args_ls = list(Y=results_ls))
+  X <- ready4show::Ready4showSynopsis(background_1L_chr = "The data reported in this document is entirely fictional.", 
+                                      coi_1L_chr = "None declared.", conclusion_1L_chr = "These fake results are not to be used to inform decision making.", 
+                                      digits_int = 3L, ethics_1L_chr = "No ethics approval has been received for the work summarised in this report.", 
+                                      funding_1L_chr = "No funding information is available for this report.", 
+                                      interval_chr = NA_character_, keywords_chr = c("entirely", 
+                                                                                     "fake", "do", "not", "cite"), outp_formats_chr = "PDF", 
+                                      sample_desc_1L_chr = NA_character_, title_1L_chr = "A hypothetical report from the avert compuational model")
+  X <- renewSlot(X, "authors_r3", first_nm_chr = "Anonymous", 
+                 middle_nm_chr = "", last_nm_chr = "Author", title_chr = "", 
+                 qualifications_chr = "", institute_chr = "Institute_A", 
+                 sequence_int = 1, is_corresponding_lgl = T, email_chr = "fake_email@fake_institute.com")
+  X <- renewSlot(X, "institutes_r3", short_name_chr = "Institute_A", 
+                 long_name_chr = "Anonymous Institute")
+  X <- renewSlot(X, "a_Ready4showPaths@outp_data_dir_1L_chr", 
+                 output_dir_1L_chr)
+  X <- renewSlot(X, "a_Ready4showPaths@mkdn_source_dir_1L_chr", 
+                 mkdn_source_dir_1L_chr)
+  authorData(X, consent_1L_chr = consent_1L_chr, consent_indcs_int = consent_indcs_int, 
+             options_chr = options_chr, tmpl_url_1L_chr = "https://github.com/avertableburden/avert_report", 
+             tmpl_version_1L_chr = "0.1.2", what_1L_chr = "Manuscript")
+  authorReport(X, consent_1L_chr = consent_1L_chr, args_ls = list(Y = results_ls))
 }
-avert::write_report(avert::get_input_data(), consent_1L_chr = "Y", mkdn_source_dir_1L_chr = getwd())
-# X <- renewSlot(X,"a_Ready4showPaths@outp_data_dir_1L_chr",
-#                "Tests")
-# authorData(X,
-#            consent_1L_chr = consent_1L_chr,
-#            consent_indcs_int = consent_indcs_int,
-#            options_chr = options_chr,
-#            tmpl_url_1L_chr = "",
-#            tmpl_version_1L_chr = "",
-#            what_1L_chr = "Manuscript")
-# 
-# x<-X
-# library(ready4show)
-# header_yaml_args_ls <- make_header_yaml_args_ls(x@authors_r3,
-#                                                 institutes_tb = x@institutes_r3,
-#                                                 keywords_chr = x@keywords_chr,
-#                                                 title_1L_chr = x@title_1L_chr)
-# write_header_fls(path_to_header_dir_1L_chr = paste0("Tests/Markdown",
-#                                                     "/Header"),
-#                  header_yaml_args_ls = header_yaml_args_ls,
-#                  abstract_args_ls = x@abstract_args_ls,
-#                  consent_1L_chr = consent_1L_chr,
-#                  consent_indcs_int = consent_indcs_int,
-#                  options_chr = options_chr)
-# rmarkdown::render(input = "Tests/Markdown/Parent_PDF/Test.Rmd",
-#                   output_format = NULL,
-#                   params = list(X=X),
-#                   output_file = "Report.pdf",
-#                   output_dir = "Tests/Report")
+write_avert_report(get_input_data(), consent_1L_chr = "Y", mkdn_source_dir_1L_chr = getwd())
+
