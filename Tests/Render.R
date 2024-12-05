d184de2ea9a27fe42e36d9d6004230fd078162dc
Matthew P Hamilton <30450935+matthewphamilton@users.noreply.github.com>
2024-12-05 13:19:48 +1100

    pre release edit

--- Tests/Render.R
+++ Tests/Render.R
@@ -1,6 +1,8 @@
-write_report <- function(consent_1L_chr = "",
+write_report <- function(results_ls,
+                         consent_1L_chr = "",
                          consent_indcs_int = 1L,
                          markdown_dir_1L_chr = character(0),
+                         mkdn_source_dir_1L_chr = NA_character_,
                          options_chr = c("Y", "N"),
                          output_dir_1L_chr = character(0)){
   if(identical(markdown_dir_1L_chr, character(0))|identical(output_dir_1L_chr, character(0))){
@@ -25,43 +27,56 @@ write_report <- function(consent_1L_chr = "",
                                       title_1L_chr = "A hypothetical report from the avert compuational model")
   X <- renewSlot(X,
                  "authors_r3",
-                 first_nm_chr = "FirstName",
+                 first_nm_chr = "Anonymous",
                  middle_nm_chr = "",
-                 last_nm_chr = "FamilyNAme",
-                 title_chr = "Dr",
-                 qualifications_chr = "MD, PhD",
-                 institute_chr = "Institute_A, Institute_B",
+                 last_nm_chr = "Author",
+                 title_chr = "",
+                 qualifications_chr = "",
+                 institute_chr = "Institute_A",
                  sequence_int = 1,
                  is_corresponding_lgl = T,
                  email_chr = "fake_email@fake_institute.com")  
   X <- renewSlot(X,
-                 "institutes_r3", short_name_chr = "Institute_A", long_name_chr = "Institute A, Australia") %>%
-    renewSlot("institutes_r3", short_name_chr = "Institute_B", long_name_chr = "Institute B, Australia")
+                 "institutes_r3", short_name_chr = "Institute_A", long_name_chr = "Anonymous Institute") 
+  #
+ X <- renewSlot(X,"a_Ready4showPaths@outp_data_dir_1L_chr",
+              temp_dir_1L_chr)
+ X <- renewSlot(X, "a_Ready4showPaths@mkdn_source_dir_1L_chr", mkdn_source_dir_1L_chr)
   authorData(X,
              consent_1L_chr = consent_1L_chr,
              consent_indcs_int = consent_indcs_int,
              options_chr = options_chr,
              tmpl_url_1L_chr = "https://github.com/avertableburden/avert_report",
-             tmpl_version_1L_chr = "0.0.1",
+             tmpl_version_1L_chr = "0.1.2",
              what_1L_chr = "Manuscript")
-  authorReport(X, consent_1L_chr = consent_1L_chr)
-  
+  authorReport(X, consent_1L_chr = consent_1L_chr, args_ls = list(Y=results_ls))
 }
-x<-X
-library(ready4show)
-header_yaml_args_ls <- make_header_yaml_args_ls(x@authors_r3,
-                                                institutes_tb = x@institutes_r3,
-                                                keywords_chr = x@keywords_chr,
-                                                title_1L_chr = x@title_1L_chr)
-write_header_fls(path_to_header_dir_1L_chr = paste0("Tests/Markdown",
-                                                    "/Header"),
-                 header_yaml_args_ls = header_yaml_args_ls,
-                 abstract_args_ls = x@abstract_args_ls,
-                 consent_1L_chr = consent_1L_chr,
-                 consent_indcs_int = consent_indcs_int,
-                 options_chr = options_chr)
-rmarkdown::render(input = "Tests/Markdown/Parent_PDF/Test.Rmd",
-                  output_format = NULL,
-                  params = list(X=X),
-                  output_file = "Report.pdf",
-                  output_dir = "Tests/Report")
+avert::write_report(avert::get_input_data(), consent_1L_chr = "Y", mkdn_source_dir_1L_chr = getwd())
+# X <- renewSlot(X,"a_Ready4showPaths@outp_data_dir_1L_chr",
+#                "Tests")
+# authorData(X,
+#            consent_1L_chr = consent_1L_chr,
+#            consent_indcs_int = consent_indcs_int,
+#            options_chr = options_chr,
+#            tmpl_url_1L_chr = "",
+#            tmpl_version_1L_chr = "",
+#            what_1L_chr = "Manuscript")
+# 
+# x<-X
+# library(ready4show)
+# header_yaml_args_ls <- make_header_yaml_args_ls(x@authors_r3,
+#                                                 institutes_tb = x@institutes_r3,
+#                                                 keywords_chr = x@keywords_chr,
+#                                                 title_1L_chr = x@title_1L_chr)
+# write_header_fls(path_to_header_dir_1L_chr = paste0("Tests/Markdown",
+#                                                     "/Header"),
+#                  header_yaml_args_ls = header_yaml_args_ls,
+#                  abstract_args_ls = x@abstract_args_ls,
+#                  consent_1L_chr = consent_1L_chr,
+#                  consent_indcs_int = consent_indcs_int,
+#                  options_chr = options_chr)
+# rmarkdown::render(input = "Tests/Markdown/Parent_PDF/Test.Rmd",
+#                   output_format = NULL,
+#                   params = list(X=X),
+#                   output_file = "Report.pdf",
+#                   output_dir = "Tests/Report")
